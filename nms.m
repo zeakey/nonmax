33da4987679351c80c4e55843aed45676da2da0f
KAI ZHAO <zhaok1206@gmail.com>
2017-12-19 15:46:38 +0800

    normalize

--- nms.m
+++ nms.m
@@ -1,6 +1,7 @@
 function [ E1 ] = nms( E )
 %NMS Summary of this function goes here
 %   Detailed explanation goes here
+E = single(E); E = E - min(E(:)); E = E/ max(E(:));
 [Ox,Oy]=gradient2(convTri(E,4));
 [Oxx,~]=gradient2(Ox); [Oxy,Oyy]=gradient2(Oy);
 O=mod(atan(Oyy.*sign(-Oxy)./(Oxx+1e-5)),pi);
